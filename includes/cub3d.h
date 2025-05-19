d460f1d5faf5d9e46cb79ebd019fb4abc994c1a7
Arthur <arthur4460011@gmail.com>
2025-05-19 17:26:57 +0200

    Added RGB struct and valgrindrc

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,11 +6,25 @@
 /*   By: aperceva <aperceva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:55:14 by aperceva          #+#    #+#             */
-/*   Updated: 2025/05/16 16:57:06 by aperceva         ###   ########.fr       */
+/*   Updated: 2025/05/19 16:43:32 by aperceva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "libft.h"
+#include <stdio.h>
+#include "MLX42.h"
+#include <math.h>
 
+typedef struct {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} ColorRGB;
+
+const ColorRGB RGB_Red    = {255, 0, 0};
+const ColorRGB RGB_Green  = {0, 255, 0};
+const ColorRGB RGB_Blue   = {0, 0, 255};
+const ColorRGB RGB_White  = {255, 255, 255};
+const ColorRGB RGB_Yellow = {255, 255, 0};
--- src/main.c
+++ src/main.c
@@ -6,37 +6,32 @@
 /*   By: aperceva <aperceva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:20:03 by arthur            #+#    #+#             */
-/*   Updated: 2025/05/16 17:32:13 by aperceva         ###   ########.fr       */
+/*   Updated: 2025/05/19 17:08:03 by aperceva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// Written by Bruh
-
-#include <stdio.h>
-#include "MLX42.h"
-
-#define WIDTH 1280
-#define HEIGHT 720
+#define mapWidth 24
+#define mapHeight 24
+#define screenWidth 640
+#define screenHeight 480
 
 static void ft_error(void)
 {
 	ft_dprintf(STDERR_FILENO, "%s", mlx_strerror(mlx_errno));
 	exit(EXIT_FAILURE);
 }
-
-int32_t	main(void)
+int main(void)
 {
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "cub3d", true);
+	mlx_t* mlx = mlx_init(screenWidth, screenHeight, "cub3d", true);
 	if (!mlx)
 		ft_error();
-
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	mlx_image_t* img = mlx_new_image(mlx, 640, 480);
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_error();
-
+			ft_error();
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
+
